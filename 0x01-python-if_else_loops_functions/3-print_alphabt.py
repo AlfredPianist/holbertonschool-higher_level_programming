1290914becfd1423e7b1886b834cd6d04a117024
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-04-27 19:06:17 +0000

    Fix tasks 2 and 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i != ord('e'):
-        print("{}".format(chr(i)), end='')
+print("".format()
+      .join([chr(i) for i in range(ord('a'), ord('z') + 1)
+             if i != ord('e') and i != ord('q')]),
+      end="")
