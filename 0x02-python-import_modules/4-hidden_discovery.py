dcb3fccbbc4312cdc1241fd87a165fddf000f89f
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-04-28 22:57:48 +0000

    Delete: hidden_4 decompiled module and dir solution task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    from inspect import getmembers, isfunction
 
-    for f in getmembers(hidden_4):
-        if isfunction(f[1]):
-            print("{}".format(f[0]))
+    for f in dir(hidden_4):
+        if not f.startswith("__"):
+            print("{}".format(f))
