476bcb92857d33473669efe1be45fb3a674c3184
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-05-05 17:21:07 +0000

    Fix: 9 from 0x03 - forgot negative ints

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_i = 0
-
     if my_list:
+        max_i = my_list[0]
         for i in my_list:
-            max_i = i if i > max_i else max_i
+            max_i = i if i >= max_i else max_i
         return max_i
     return None
