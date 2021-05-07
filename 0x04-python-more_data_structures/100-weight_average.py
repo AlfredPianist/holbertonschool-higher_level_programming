8317eb26dd0818bec66bc8839d8c2d445e1c3b95
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-05-07 00:15:35 +0000

    Add: task 102 from 0x04

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    res = 0
     if len(my_list) == 0:
-        return res
+        return 0
 
     return sum([t[0] * t[1] for t in my_list]) / sum([t[1] for t in my_list])
