6d851e052959a550d266b387a4ee976778d4362e
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-05-17 18:32:57 +0000

    Add: task 100 from 0x05

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
-        
+        return True
+    except BaseException:
+        exc_value = sys.exc_info()[1]
+        print("Exception:", exc_value, file=sys.stderr)
+        return False
