adae0dcacb81df4a02b7a8fcc4d3ded12c82777e
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-05-19 00:42:31 +0000

    Add: task 100 from 0x06

---
+++ 0x06-python-classes/100-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
+Node = __import__('100-singly_linked_list').Node
+
+sll = SinglyLinkedList()
+sll.sorted_insert(2)
+sll.sorted_insert(5)
+sll.sorted_insert(3)
+sll.sorted_insert(10)
+sll.sorted_insert(1)
+sll.sorted_insert(-4)
+sll.sorted_insert(-3)
+sll.sorted_insert(4)
+sll.sorted_insert(5)
+sll.sorted_insert(12)
+sll.sorted_insert(3)
+print(sll)
