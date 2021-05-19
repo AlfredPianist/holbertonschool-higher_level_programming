c175e44be18a65ecd3917787f86914f509aeb0b6
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-05-19 01:35:34 +0000

    Fix: task 103 from 0x06 - Default 0 radius if no parameter

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ import math
 
 class MagicClass:
     """MagicClass. A Circle Class."""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """Initialization of MagicClass object with its radius."""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
