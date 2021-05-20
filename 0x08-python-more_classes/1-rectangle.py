503056c3ad336e8a606cab9d95d5f908869b0bbb
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-05-20 23:45:02 +0000

    Fix: value names setter 1 from 0x08

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,12 +27,12 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
+    def width(self, width):
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self.__width = width
 
     @property
     def height(self):
@@ -47,9 +47,9 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
+    def height(self, height):
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self.__height = height
