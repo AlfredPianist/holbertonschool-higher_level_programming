01ca687bc0eacc9d1699497cf5415c1902c40c9f
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-07-12 20:17:07 +0000

    Fix: Actually count id 89 task 8 from 0x0D

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,6 @@
 -- Insert row with id 89 and name Holberton School to table first_table
 --   in database passed as argument of the mysql command.
-SELECT COUNT(*) FROM first_table;
+SELECT COUNT(*)
+FROM
+	first_table
+WHERE id = 89;
