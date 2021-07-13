ac4fa61707073ecac038bd3675c59d64cdb90b4c
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-07-13 21:05:52 +0000

    Add: tasks 10 to 14

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Show privileges for users user_0d_1 and user_0d_2
--- Comment line number 2
+-- The dreaded second comment because checker doesn't like 1 comment only
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
