5b80be366d77942f915a64d6332039706505818e
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-07-13 21:42:40 +0000

    Fix: NOT NULL condition task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,6 +4,7 @@ SELECT
 	tv_genres.name AS genre,
 	COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
-GROUP BY tv_genres.name
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+GROUP BY genre
 ORDER BY number_of_shows DESC;
