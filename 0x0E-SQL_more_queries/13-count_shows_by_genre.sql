503736edc9fd71d93c7fa038f1d435330928176b
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-07-13 21:17:21 +0000

    Fix: task 13, comment?

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
 -- Prints all genres with its number of shows associated with them.
+-- The dreaded second comment because checker doesn't like 1 comment only.
 SELECT
 	tv_genres.name AS genre,
 	COUNT(tv_show_genres.show_id) AS number_of_shows
