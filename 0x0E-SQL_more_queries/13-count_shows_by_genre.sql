7df98a8f1c6d34f09ecdc0c919309a2dfe95470d
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-07-13 21:10:42 +0000

    Fix: 13 from 0x0E

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Prints all genres with its number of shows associated with them.
 SELECT
-	tv_genres.name,
+	tv_genres.name AS genre,
 	COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
