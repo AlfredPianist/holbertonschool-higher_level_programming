2ee314c2096ff554a294e7fb9bf56213c9170fd1
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-02 19:26:38 +0000

    Fix: Typo on 1, only first argument on 3.

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 // Prints 3 messages to the console
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,7 +5,5 @@ const { argv } = require('process');
 if (argv[2] === undefined) {
   console.log('No argument');
 } else {
-  argv.slice(2).forEach((val) => {
-    console.log(val);
-  });
+  console.log(argv[3]);
 }
