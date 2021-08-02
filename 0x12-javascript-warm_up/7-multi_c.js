ac64e6095cef3b769936c720380d4548dd4f2b8b
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-02 20:31:25 +0000

    Fix: negative numbers task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const { argv } = require('process');
 const num = parseInt(argv[2]);
 if (isNaN(num)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (num > 0) {
   const iterator = Array.apply([], Array(num));
   iterator.forEach(() => console.log('C is fun'));
 }
