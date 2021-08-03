cf27af5b3fd5147437fcb49128e041f9db33dbd1
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-03 21:14:48 +0000

    Add: task 6 from 0x13

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/*
+  Creation of square class inheriting from Rectangle with size and
+  method charPrint.
+*/
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    const glyph = !c ? 'X' : c;
+    const iterator = Array.apply([], Array(this.height));
+    iterator.forEach(() => console.log(glyph.repeat(this.width)));
+  }
+}
+module.exports = Square;
