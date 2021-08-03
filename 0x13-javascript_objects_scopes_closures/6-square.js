7145564e6f5c4892f5f6d6bf804a36085e7f346f
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-03 21:16:31 +0000

    Fix: 6 from 0x13. Equality to undefined

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -11,7 +11,7 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
-    const glyph = !c ? 'X' : c;
+    const glyph = c === undefined ? 'X' : c;
     const iterator = Array.apply([], Array(this.height));
     iterator.forEach(() => console.log(glyph.repeat(this.width)));
   }
