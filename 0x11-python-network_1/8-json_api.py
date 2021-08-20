aa05dcfa2226f7d090113651473a10762caaa902
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-20 21:45:54 +0000

    Add: task 8 for 0x11

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    letter = ""
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        letter = argv[1]
+    except IndexError:
+        pass
+
+    response = requests.post(url, data={'q': letter})
+    response = response.json()
+
+    try:
+        if (response):
+            print("[{}] {}".format(response['id'], response['name']))
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
