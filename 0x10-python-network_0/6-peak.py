270ad21549e05e189520b4debb6e79ea51b32e76
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-20 01:18:04 +0000

    Fix: efficiency 6 from 0x10

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,19 +7,26 @@ def find_peak(list_of_integers):
     if (type(list_of_integers) != list or len(list_of_integers) == 0):
         return 0
     list_len = len(list_of_integers)
-    return find_peak_helper(list_of_integers, 0, list_len - 1)
+    return find_peak_helper(list_of_integers, list_len)
 
 
-def find_peak_helper(int_list, low_idx, up_idx):
+def find_peak_helper(int_list, up_idx):
     """Helper recursive function"""
-    mid = up_idx // 2 \
-        if ".0" in str(up_idx / 2) else up_idx // 2 + 1
-    if low_idx > up_idx or len(set(int_list)) == 1:
+    mid = int(up_idx / 2)
+
+    if (up_idx == 1):
+        return int_list[0]
+    elif (up_idx == 2):
         return max(int_list)
-    elif (int_list[mid] <= int_list[mid - 1]):
-        int_list = int_list[low_idx:mid]
-        return find_peak_helper(int_list, 0, mid)
-    elif (int_list[mid] < int_list[mid + 1]):
-        int_list = int_list[mid + 1:up_idx + 1]
-        return find_peak_helper(int_list, mid + 1, len(int_list) - 1)
-    return int_list[mid]
+
+    peak = int_list[mid]
+    peak_left = int_list[mid - 1]
+    peak_right = int_list[mid + 1]
+
+    if peak_left < peak and peak > peak_right:
+        return peak
+    elif (peak_left > peak):
+        return find_peak_helper(int_list[:mid], mid)
+    else:
+        int_list = int_list[mid + 1:]
+        return find_peak_helper(int_list, len(int_list))
