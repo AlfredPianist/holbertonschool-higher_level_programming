fee42f9aea54848ba472427baab3c6fc23b03110
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-09-23 01:39:00 +0000

    Fix: semistandard

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,13 +1,13 @@
 function translate (lang) {
-    const url = 'https://www.fourtonfish.com/hellosalut/?lang='
-    $.get(`${url}${lang}`, (data, status) => {
-        $('#hello').text(data.hello);
-    });
-} 
+  const url = 'https://www.fourtonfish.com/hellosalut/?lang=';
+  $.get(`${url}${lang}`, (data, status) => {
+    $('#hello').text(data.hello);
+  });
+}
 
-$(document).ready(() => { 
-    $('#btn_translate').on ('click', () => {
-        const lang = $('#language_code').val();
-        translate(lang);
-    });
-})
+$(document).ready(() => {
+  $('#btn_translate').on('click', () => {
+    const lang = $('#language_code').val();
+    translate(lang);
+  });
+});
